432c5e4ffd3a431194204f9814b345d48196350b
Devansh Mital <devanshmittal271@gmail.com>
2026-07-01 00:36:48 +0530

    Refactor minimum window substring algorithm

--- SlidingWindow/76_MinimumWindowSubstring.cpp
+++ SlidingWindow/76_MinimumWindowSubstring.cpp
@@ -4,57 +4,41 @@ public:
 
         unordered_map<char,int> need;
         unordered_map<char,int> window;
-
-        // build need map
-        for(char ch : t)
+        for (int i =0; i<t.size(); i++){
+            char ch = t[i];
             need[ch]++;
+        }
 
         int left = 0;
-        int matchedChars = 0;
-
+        int matchedChar = 0;
         int minLen = INT_MAX;
         int start = 0;
 
-        for(int right = 0; right < s.size(); right++)
-        {
+        for(int right = 0; right < s.size(); right++){
             char ch = s[right];
 
-            // expand
             window[ch]++;
-
-            if(need.count(ch) &&
-               window[ch] == need[ch])
-            {
-                matchedChars++;
+            if (need.count(ch) && window[ch] == need[ch]){
+                matchedChar++;
             }
-
-            // valid window
-            while(matchedChars == need.size())
-            {
-                // update answer
-                if(right - left + 1 < minLen)
-                {
-                    minLen = right - left + 1;
+            while (matchedChar == need.size()){
+                int windowSize = right - left +1;
+                if (right - left +1 < minLen){
+                    minLen  = windowSize;
                     start = left;
                 }
-
                 char leftChar = s[left];
-
-                // shrink
-                if(need.count(leftChar) &&
-                   window[leftChar] == need[leftChar])
-                {
-                    matchedChars--;
+                if (need.count(leftChar) && window[leftChar] == need[leftChar]){
+                    matchedChar--;
                 }
-
                 window[leftChar]--;
                 left++;
             }
+
         }
-
-        if(minLen == INT_MAX)
-            return "";
-
+        if (minLen == INT_MAX)  return "";
         return s.substr(start, minLen);
+
     }
 };
+
